c12855d7ffc40d2744732075cd49ae93e4eba4d9
pranav 2002 <pranavidhya28@gmail.com>
2022-10-10 16:07:12 +0530

    coloured menu

--- src/Menu.java
+++ src/Menu.java
@@ -1,28 +1,47 @@
+import java.awt.Color;
+import java.awt.Font;
+
 import javax.swing.*;
 
 public class Menu extends JFrame{
 	Menu(){
+		
+		Font  f1  = new Font(Font.DIALOG,  Font.BOLD, 16);
+		
 		JLabel title = new JLabel("Menu");
-		title.setBounds(180, 30, 40, 20);
+		title.setBounds(180, 40, 80, 20);
+		title.setForeground(Color.yellow);
+		title.setFont(f1);
 		add(title);
-			
+		
+		Color purp = new Color(122,136,222);
+		this.getContentPane().setBackground(purp);
+		
 		JLabel foodName = new JLabel("Food Name:");
 		foodName.setBounds(100, 75, 100, 30);
+		foodName.setForeground(Color.yellow);
+		foodName.setFont(f1);
 		add(foodName);
+		
 		JTextField nameInput = new JTextField();
 		nameInput.setBounds(220, 75, 100, 30);
 		add(nameInput);
 
 		JLabel foodPrice = new JLabel("Price:");
 		foodPrice.setBounds(100, 125, 100, 30);
+		foodPrice.setForeground(Color.yellow);
+		foodPrice.setFont(f1);
 		add(foodPrice);
+		
 		JTextField priceInput = new JTextField();
 		priceInput.setBounds(220, 125, 100, 30);
 		add(priceInput);
 
 		JButton addFood = new JButton("Add");
-		addFood.setBounds(180, 170, 75, 50);
+		addFood.setBounds(180, 190, 100, 40);
+		addFood.setBackground(Color.yellow);
 		add(addFood);
+		
 		
 		setLayout(null);
 		setVisible(true);
