473fd7916af81165f406dde422198a88ac012702
mohammedsinank <115466956+mohammedsinank@users.noreply.github.com>
2022-10-11 11:15:24 +0530

    added homepage

---
+++ src/homepage.java
@@ -0,0 +1,30 @@
+package bootathon;
+import javax.swing.*;
+import java.awt.*; 
+
+public class homepage
+{	
+	public static void main(String args[])  
+	    {  
+		    JFrame f;
+		    JLabel app_name;
+		    JButton b1;
+		    JButton b2;
+		      
+		    f=new JFrame("HOME PAGE");
+		    app_name=new JLabel("MEGA BITE");
+		    app_name.setFont(new Font("Verdana", Font.PLAIN, 18));
+		    app_name.setBounds(180,50,150,100); 
+		    b1=new JButton("REGISTER");
+		    b1.setBounds(120,150,100,40);
+		    b2=new JButton("LOGIN");
+		    b2.setBounds(250,150,100,40);
+
+		    f.add(app_name);
+		    f.add(b1);
+		    f.add(b2);
+		    f.setSize(500,400);  
+		    f.setLayout(null);  
+		    f.setVisible(true);  
+	    }  
+}
